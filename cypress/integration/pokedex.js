9ebcf4855fed8e9c3456b04b38a8cece19c7604f
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-03-22 23:00:11 -0400

    moar changes, super nice

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -8,7 +8,7 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
+    cy.contains(/ivysaur/i)
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
